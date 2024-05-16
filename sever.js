267d3701a9af76381f9fa672f43d75678ce29dc1
srikanth mekala <125475567+srikanthmekala500@users.noreply.github.com>
2024-05-16 19:56:19 +0530

    Update sever.js

--- sever.js
+++ sever.js
@@ -7,10 +7,10 @@ const connectdb = require('./db/db')
 const body_parser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
-const {fileURLToPath} = require('url')
+// const {fileURLToPath} = require('url')
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 app.use(express.json())
 app.use(cors())
 app.use(body_parser.json())
@@ -18,15 +18,15 @@ app.use(body_parser.json())
 connectdb()
  
 //static file
-app.use(express.static(path.join(__dirname,'./bet/build'))) 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./bet/build/index.html'))
+app.use(express.static(path.join(__dirname,"./bet/build")))
+app.get("*",function(req,res){
+res.sendFile(path.join(__dirname,"./bet/build/index.html"))
 })
 
 app.use('/api/v1/user',require('./routers/usertouter'))
 
 app.listen(port,()=>{
-    console.log(`Server Running On PORT ${port} on ${process.env.NODE_ENV} Mode `
+    console.log(`Server Running On PORT ${process.env.DEV_MODEL} on ${process.env.NODE_ENV} Mode `
     .bgMagenta.white
     )
 })
